8bdcd451b06e5460133def1adf5d0e8a88d3ec37
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-01-22 19:14:09 -0600

    added js and html

---
+++ boybands.js
@@ -0,0 +1,27 @@
+let bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
+let vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
+
+// Get a reference to the appropriate DOM element for bands
+const bandElement = document.getElementById("boy-bands");
+
+// Get a reference to the appropriate DOM element for vegetables
+const veggieElement = document.getElementById("vegetables");
+
+// Execute a for loop that will iterate over the arrays
+for (let i = 0; i < 5; i += 1) {
+
+// Get a reference to the current item in the bands array
+const currentBand = bands[i] + "<br>";
+console.log(currentBand);
+
+// Update the innerHTML value of the DOM element for bands
+bandElement.innerHTML += currentBand;
+
+// Get a reference to the current item in the vegetables array
+const currentVeggie = vegetables[i] + "<br>";
+console.log(currentVeggie);
+
+// Update the innerHTML value of the DOM element for vegetables
+veggieElement.innerHTML += currentVeggie;
+
+}
